30c90984c8cfc3928cf5ba5ba5be9dcc757b6393
Kate Hunt <katehunt789@gmail.com>
2020-05-03 18:47:35 +0100

    Form completion

--- js/scripts.js
+++ js/scripts.js
@@ -215,9 +215,8 @@ $(document).ready(function () {
 
     /********************** RSVP **********************/
     $('#rsvp-form').on('submit', function (e) {
-        $('#rsvp-modal').modal('show');
         e.preventDefault();
-        var data = $(this).serialize();
+        var user = $(this).serialize();
 
         $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
 
@@ -225,9 +224,8 @@ $(document).ready(function () {
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbzc9niNrr-2zN8SHcw67ubUFJi3IeiOrRlPpKicOREbnjDCuPA/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbzc9niNrr-2zN8SHcw67ubUFJi3IeiOrRlPpKicOREbnjDCuPA/exec', user)
                 .done(function (data) {
-                    console.log(data);
                     if (data.result === "error") {
                         $('#alert-wrapper').html(alert_markup('danger', data.message));
                     } else {
@@ -235,8 +233,8 @@ $(document).ready(function () {
                         $('#rsvp-modal').modal('show');
                     }
                 })
-                .fail(function (data) {
-                    console.log(data);
+                .fail(function (err) {
+                    console.log('error', err);
                     $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> There is some issue with the server. '));
                 });
         }
