ffaf844dd3d6fc62907aaf172ba86e8306da480d
Kate Hunt <katehunt789@gmail.com>
2020-12-13 11:25:17 +0000

    Fix header

--- js/scripts.js
+++ js/scripts.js
@@ -135,28 +135,29 @@ $(document).ready(function () {
 
             if (scroll >= 20) {
                 $('div.navigation').addClass('fixed');
-                $('header').css({
-                    "border-bottom": "none",
-                    "padding": "35px 0"
-                });
-                $('header .member-actions').css({
-                    "top": "26px",
-                });
-                $('header .navicon').css({
-                    "top": "24px",
-                });
+                // $('header').css({
+                //     "border-bottom": "none",
+                //     "padding": "35px 0"
+                // });
+                // $('header .member-actions').css({
+                //     "top": "26px",
+                // });
+                // $('header .navicon').css({
+                //     "top": "24px",
+                // });
             } else {
+                console.log(scroll);
                 $('div.navigation').removeClass('fixed');
-                $('header').css({
-                    "border-bottom": "solid 1px rgba(255, 255, 255, 0.2)",
-                    "padding": "50px 0"
-                });
-                $('header .member-actions').css({
-                    "top": "41px",
-                });
-                $('header .navicon').css({
-                    "top": "48px",
-                });
+                // $('header').css({
+                //     "border-bottom": "solid 1px rgba(255, 255, 255, 0.2)",
+                //     "padding": "50px 0"
+                // });
+                // $('header .member-actions').css({
+                //     "top": "41px",
+                // });
+                // $('header .navicon').css({
+                //     "top": "48px",
+                // });
             }
         });
     });
